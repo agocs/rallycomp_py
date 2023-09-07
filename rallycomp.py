23fca93262464ba606154b2e11da7f14ab1a78a7
Chris Agocs <chris@agocs.org>
2023-09-06 23:15:00 -0700

    Add odometer and CAST

--- rallycomp.py
+++ rallycomp.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 from typing import Tuple
 
+
 class FourDPosition:
     def __init__(self, position: Tuple[float, float], alt: float, timestamp: datetime):
         self.lat = position[0]
@@ -12,38 +13,42 @@ class FourDPosition:
         self.timestamp = timestamp
 
     def distance_between_two_gps_points(self, lat1, lon1, lat2, lon2):
-        R = 6371 # Radius of the earth in km
-        dLat = self.deg2rad(lat2-lat1) # deg2rad below
-        dLon = self.deg2rad(lon2-lon1)
-        a = math.sin(dLat/2) * math.sin(dLat/2) + \
-            math.cos(self.deg2rad(lat1)) * math.cos(self.deg2rad(lat2)) * \
-            math.sin(dLon/2) * math.sin(dLon/2)
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-        d = R * c # Distance in km
-        return d * 1000 # Distance in m
-    
+        R = 6371  # Radius of the earth in km
+        dLat = self.deg2rad(lat2 - lat1)  # deg2rad below
+        dLon = self.deg2rad(lon2 - lon1)
+        a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(
+            self.deg2rad(lat1)
+        ) * math.cos(self.deg2rad(lat2)) * math.sin(dLon / 2) * math.sin(dLon / 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        d = R * c  # Distance in km
+        return d * 1000  # Distance in m
+
     def deg2rad(self, deg):
-        return deg * (math.pi/180)
+        return deg * (math.pi / 180)
 
     def subtract(self, other):
-        horiz = self.distance_between_two_gps_points(self.lat, self.lon, other.lat, other.lon)
+        horiz = self.distance_between_two_gps_points(
+            self.lat, self.lon, other.lat, other.lon
+        )
         vert = other.alt - self.alt
         magnitude = math.sqrt(horiz**2 + vert**2)
         time_diff = self.timestamp - other.timestamp
         return Displacement(magnitude, time_diff)
 
+
 class Displacement:
     def __init__(self, distance, time):
         self.distance = distance
-        self.time = time 
+        self.time = time
+
 
 class Odometer:
-    def __init__(self, origFix: FourDPosition):
+    def __init__(self, origFix: FourDPosition, calibration: float = 1):
         self.origFix = origFix
         self.distanceAccumulator = 0
         self.lastFix = origFix
 
-    def addPosition(self, newFix:FourDPosition):
+    def addPosition(self, newFix: FourDPosition):
         displacement = newFix.subtract(self.lastFix)
         self.distanceAccumulator = self.distanceAccumulator + displacement.distance
         self.lastFix = newFix
@@ -53,13 +58,31 @@ class Odometer:
         hours = elapsed.total_seconds() / 60 / 60
         return self.distanceAccumulator / 1000 / hours  # kilometers per hour
 
+    def get_elapsed_time(self):
+        return self.lastFix.timestamp - self.origFix.timestamp
+
+
+class CAST:
+    def __init__(self, average: float, odo: Odometer):
+        self.average = average
+        self.odo = odo
+
+    def get_offset(self):
+        ideal = (
+            self.odo.get_elapsed_time().total_seconds() / 60 / 60
+        ) * self.average  #  kilometers
+        actual = self.odo.distanceAccumulator / 1000  # kilometers
+        differential = ideal - actual
+        return differential / self.average * 60 * 60  # seconds
+
 
 def block_until_new_fix(last_position):
     packet = gpsd.get_current()
     while packet.get_time() == last_position.timestamp:
-        time.sleep(.1)
+        time.sleep(0.1)
         packet = gpsd.get_current()
     return packet
+
 
 def main_loop():
     gpsd.connect()
@@ -70,15 +93,20 @@ def main_loop():
         print(f"Waiting for fix... (mode: {packet.mode})")
     print("Fix acquired!")
 
-    odometer = 0
-    last_position = FourDPosition(packet.position(), packet.altitude(), packet.get_time())
+    odo = Odometer(
+        FourDPosition((packet.lat, packet.lon), packet.alt, packet.get_time())
+    )
+    cast = CAST(20, odo)
+
     while True:
-        packet = block_until_new_fix(last_position)
-        current_position = FourDPosition(packet.position(), packet.altitude(), packet.get_time())
-        displacement = current_position.subtract(last_position)
-        odometer += displacement.distance
-        print(f"Odometer: {odometer}m")
-        last_position = current_position
+        block_until_new_fix(odo.lastFix)
+        packet = gpsd.get_current()
+        odo.addPosition(
+            FourDPosition((packet.lat, packet.lon), packet.alt, packet.get_time())
+        )
+        print(
+            f"Distance: {odo.distanceAccumulator / 1000} km \t Average: {odo.get_average_speed()} km/h \t CAST:{cast.average} \t Offset: {cast.get_offset()}"
+        )
 
 
 if __name__ == "__main__":
--- rallycomp_test.py
+++ rallycomp_test.py
@@ -2,59 +2,83 @@ import unittest
 from rallycomp import FourDPosition, Odometer
 from datetime import datetime
 
-class TestFourDPosition(unittest.TestCase):
 
+class TestFourDPosition(unittest.TestCase):
     def test_FourDPosition_distance_to_vertical(self):
-        position1 = FourDPosition((47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.69431, -122.345998333), 155, datetime(2020, 1, 1, 0, 0, 2, 0))
+        position1 = FourDPosition(
+            (47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0)
+        )
+        position2 = FourDPosition(
+            (47.69431, -122.345998333), 155, datetime(2020, 1, 1, 0, 0, 2, 0)
+        )
         dist = position2.subtract(position1)
         self.assertEqual(dist.distance, 5)
         self.assertEqual(dist.time.seconds, 2)
 
     def test_FourDPosition_distance_to_horizontal(self):
-        position1 = FourDPosition((47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.69432, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 2, 0))
+        position1 = FourDPosition(
+            (47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0)
+        )
+        position2 = FourDPosition(
+            (47.69432, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 2, 0)
+        )
         dist = position2.subtract(position1)
         self.assertEqual(dist.distance, 1.111949266008448)
         self.assertEqual(dist.time.seconds, 2)
 
-        position1 = FourDPosition((47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.69430, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 2, 0))
+        position1 = FourDPosition(
+            (47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0)
+        )
+        position2 = FourDPosition(
+            (47.69430, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 2, 0)
+        )
         dist = position2.subtract(position1)
-        self.assertEqual(dist.distance, 1.1119492667985353)  # shouldn't be much different
+        self.assertEqual(
+            dist.distance, 1.1119492667985353
+        )  # shouldn't be much different
         self.assertEqual(dist.time.seconds, 2)
 
     def test_FourDPosition_distance_to_diagonal(self):
-        position1 = FourDPosition((47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.69432, -122.345999333), 151, datetime(2020, 1, 1, 0, 0, 2, 0))
+        position1 = FourDPosition(
+            (47.69431, -122.345998333), 150, datetime(2020, 1, 1, 0, 0, 0, 0)
+        )
+        position2 = FourDPosition(
+            (47.69432, -122.345999333), 151, datetime(2020, 1, 1, 0, 0, 2, 0)
+        )
         dist = position2.subtract(position1)
         self.assertEqual(dist.distance, 1.49734189653602)
         self.assertEqual(dist.time.seconds, 2)
 
     def test_FourDPosition_distance_nautical_mile(self):
         position1 = FourDPosition((47.0, -122.0), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.0 + (1/60), -122.0), 151, datetime(2020, 1, 1, 0, 0, 2, 0))
+        position2 = FourDPosition(
+            (47.0 + (1 / 60), -122.0), 151, datetime(2020, 1, 1, 0, 0, 2, 0)
+        )
         dist = position2.subtract(position1)
         self.assertEqual(dist.distance, 1853.2490472056688)  # 1 nautical mile to km
-
 
 
 class TestOdometer(unittest.TestCase):
     def test_odometer(self):
         position1 = FourDPosition((47.0, -122.0), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.0 + (1/60), -122.0), 151, datetime(2020, 1, 1, 1, 0, 0, 0))
+        position2 = FourDPosition(
+            (47.0 + (1 / 60), -122.0), 151, datetime(2020, 1, 1, 1, 0, 0, 0)
+        )
         odo = Odometer(position1)
         odo.addPosition(position2)
         self.assertEqual(odo.distanceAccumulator, 1853.2490472056688)
-        self.assertEqual(odo.get_average_speed(), 1.8532490472056689) # 1 mph ish
-        
+        self.assertEqual(odo.get_average_speed(), 1.8532490472056689)  # 1 mph ish
 
     def test_odometer_more(self):
         position1 = FourDPosition((47.0, -122.0), 150, datetime(2020, 1, 1, 0, 0, 0, 0))
-        position2 = FourDPosition((47.0 + (1/60), -122.0), 151, datetime(2020, 1, 1, 1, 0, 0, 0))
-        position3 = FourDPosition((47.0 + (2/60), -122.0), 151, datetime(2020, 1, 1, 2, 0, 0, 0))
+        position2 = FourDPosition(
+            (47.0 + (1 / 60), -122.0), 151, datetime(2020, 1, 1, 1, 0, 0, 0)
+        )
+        position3 = FourDPosition(
+            (47.0 + (2 / 60), -122.0), 151, datetime(2020, 1, 1, 2, 0, 0, 0)
+        )
         odo = Odometer(position1)
         odo.addPosition(position2)
         odo.addPosition(position3)
         self.assertEqual(odo.distanceAccumulator, 3706.4978246148758)
-        self.assertEqual(odo.get_average_speed(), 1.8532489123074378) # 1 mph ish
+        self.assertEqual(odo.get_average_speed(), 1.8532489123074378)  # 1 mph ish
